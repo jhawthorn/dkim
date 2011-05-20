ca1cfb4486edd238f7636439967ac21908c6bf5f
John Hawthorn <john.hawthorn@gmail.com>
2011-05-20 10:52:09 -0700

    require specifying domain in dkimsign.rb executable

--- bin/dkimsign.rb
+++ bin/dkimsign.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/ruby
 
 if ARGV.length != 2 && ARGV.length != 3
-  puts "Usage: dkimsign.rb SELECTOR KEYFILE [MAILFILE]"
+  puts "Usage: dkimsign.rb DOMAIN SELECTOR KEYFILE [MAILFILE]"
   exit 0
 end
 
 require 'dkim'
 
-selector, keyfile,mailfile = ARGV
+domain, selector, keyfile, mailfile = ARGV
 
 keyfile  = File.open(keyfile)
 mailfile = mailfile ? File.open(mailfile) : STDIN
@@ -15,7 +15,8 @@ mailfile = mailfile ? File.open(mailfile) : STDIN
 mail = mailfile.read.gsub(/\r?\n/, "\r\n")
 key  = keyfile.read
 
-Dkim::selector    = selector
+Dkim::domain = domain
+Dkim::selector = selector
 Dkim::private_key = key
 
 print Dkim::SignedMail.new(mail).to_s
