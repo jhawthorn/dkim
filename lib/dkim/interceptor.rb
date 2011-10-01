4599cdac3dc4b80a117099b7c6ec132aee91e3a0
John Hawthorn <john.hawthorn@gmail.com>
2011-10-01 03:07:20 -0700

    temporary fix: stop Mail from folding header lines

--- lib/dkim/interceptor.rb
+++ lib/dkim/interceptor.rb
@@ -1,7 +1,9 @@
+
 module Dkim
   class Interceptor
     def self.delivering_email(message)
-      message['DKIM-Signature'] = SignedMail.new(message.encoded).dkim_header.value
+      require 'mail/dkim_field'
+      message.header.fields << Mail::DkimField.new(SignedMail.new(message.encoded).dkim_header.value)
       message
     end
   end
---
+++ lib/mail/dkim_field.rb
@@ -0,0 +1,25 @@
+
+require 'mail'
+require 'mail/fields'
+
+module Mail
+  class DkimField < StructuredField
+    FIELD_NAME = 'dkim-signature'
+    CAPITALIZED_FIELD = 'DKIM-Signature'
+
+    def initialize(value = nil, charset = 'utf-8')
+      self.charset = charset
+      super(CAPITALIZED_FIELD, strip_field(FIELD_NAME, value), charset)
+      self
+    end
+
+    def encoded
+      "#{name}:#{value}\n"
+    end
+
+    def decoded
+      "#{name}:#{value}\n"
+    end
+  end
+end
+
