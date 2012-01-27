59dffae666131249ef0fd5c4cd00d91f4ac6d1ea
John Hawthorn <john.hawthorn@gmail.com>
2012-01-27 15:30:43 -0800

    Strip old signatures in Interceptor (fixes #6)

--- lib/dkim/interceptor.rb
+++ lib/dkim/interceptor.rb
@@ -3,7 +3,17 @@ module Dkim
   class Interceptor
     def self.delivering_email(message)
       require 'mail/dkim_field'
-      message.header.fields << Mail::DkimField.new(SignedMail.new(message.encoded).dkim_header.value)
+
+      # strip any existing signatures
+      message.header.fields.reject! do |field|
+        field.name =~ /^DKIM-Signature$/i
+      end
+
+      # generate new signature
+      dkim_signature = SignedMail.new(message.encoded).dkim_header.value
+
+      # append signature to message
+      message.header.fields << Mail::DkimField.new(dkim_signature)
       message
     end
   end
