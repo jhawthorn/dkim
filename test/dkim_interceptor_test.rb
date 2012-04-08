fa8d078f48a2d51c8fb3ba9501cbc92f1b1d8801
John Hawthorn <john.hawthorn@gmail.com>
2012-04-08 02:39:10 -0700

    test header stripping in Interceptor

--- test/dkim_interceptor_test.rb
+++ test/dkim_interceptor_test.rb
@@ -2,6 +2,39 @@ require 'test_helper'
 
 require 'mail'
 
+SIGNEDMAIL = <<-eos
+DKIM-Signature: v=1; a=rsa-sha256; s=brisbane; d=example.com;
+      c=simple/simple; q=dns/txt; i=joe@football.example.com;
+      h=Received : From : To : Subject : Date : Message-ID;
+      bh=2jUSOH9NhtVGCQWNr9BrIAPreKQjO6Sn7XIkfJVOzv8=;
+      b=AuUoFEfDxTDkHlLXSZEpZj79LICEps6eda7W3deTVFOk4yAUoqOB
+        4nujc7YopdG5dWLSdNg6xNAZpOPr+kHxt1IrE+NahM6L/LbvaHut
+        KVdkLLkpVaVVQPzeRDI009SO2Il5Lu7rDNH6mZckBdrIx0orEtZV
+        4bmp/YzhwvcubU4=;
+Received: from client1.football.example.com  [192.0.2.1]
+      by submitserver.example.com with SUBMISSION;
+      Fri, 11 Jul 2003 21:01:54 -0700 (PDT)
+From: Joe SixPack <joe@football.example.com>
+To: Suzie Q <suzie@shopping.example.net>
+Subject: Is dinner ready?
+Date: Fri, 11 Jul 2003 21:00:37 -0700 (PDT)
+Message-ID: <20030712040037.46341.5F8J@football.example.com>
+DKIM-Signature: v=1; a=rsa-sha256; s=brisbane; d=example.com;
+      c=simple/simple; q=dns/txt; i=joe@football.example.com;
+      h=Received : From : To : Subject : Date : Message-ID;
+      bh=2jUSOH9NhtVGCQWNr9BrIAPreKQjO6Sn7XIkfJVOzv8=;
+      b=AuUoFEfDxTDkHlLXSZEpZj79LICEps6eda7W3deTVFOk4yAUoqOB
+        4nujc7YopdG5dWLSdNg6xNAZpOPr+kHxt1IrE+NahM6L/LbvaHut
+        KVdkLLkpVaVVQPzeRDI009SO2Il5Lu7rDNH6mZckBdrIx0orEtZV
+        4bmp/YzhwvcubU4=;
+
+Hi.
+
+We lost the game. Are you hungry yet?
+
+Joe.
+eos
+
 class DkimInterceptorTest < Test::Unit::TestCase
   def setup
     mail = EXAMPLEEMAIL.dup
@@ -19,7 +52,7 @@ class DkimInterceptorTest < Test::Unit::TestCase
     Dkim.body_canonicalization = 'relaxed'
     Dkim.signing_algorithm = 'rsa-sha256'
     Dkim::Interceptor.delivering_email(@mail)
-    dkim_header = @mail['Dkim-Signature']
+    dkim_header = @mail['DKIM-Signature']
     assert_not_nil dkim_header
     assert_includes dkim_header.to_s, 'rsa-sha256'
     assert_includes dkim_header.to_s, 's=brisbane'
@@ -36,7 +69,7 @@ class DkimInterceptorTest < Test::Unit::TestCase
     Dkim.body_canonicalization = 'simple'
     Dkim.signing_algorithm = 'rsa-sha256'
     Dkim::Interceptor.delivering_email(@mail)
-    dkim_header = @mail['Dkim-Signature']
+    dkim_header = @mail['DKIM-Signature']
     assert_not_nil dkim_header
     assert_includes dkim_header.to_s, 'rsa-sha256'
     assert_includes dkim_header.to_s, 's=brisbane'
@@ -46,6 +79,16 @@ class DkimInterceptorTest < Test::Unit::TestCase
     assert_includes dkim_header.to_s, 'bh=2jUSOH9NhtVGCQWNr9BrIAPreKQjO6Sn7XIkfJVOzv8='
 
     # TODO: double check signing of 'b' header
+  end
+
+  def test_strips_exsting_headers
+    @mail = Mail.new(SIGNEDMAIL)
+
+    assert_equal 2, @mail.encoded.scan('DKIM-Signature').count
+
+    Dkim::Interceptor.delivering_email(@mail)
+
+    assert_equal 1, @mail.encoded.scan('DKIM-Signature').count
   end
 end
 
